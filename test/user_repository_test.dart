ecea70724a9579c30f1d77f8c9419b62907af290
Patrick Schemel <Patrick@schemel.dk>
2023-05-28 13:56:16 +0200

    Added Google Maps Support and Begun On Page

--- test/user_repository_test.dart
+++ test/user_repository_test.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter_test/flutter_test.dart';
 import 'package:radon_app/repositories/user_logger_repository.dart';
 import 'package:radon_app/repositories/user_repository.dart';
@@ -7,7 +5,6 @@ import 'package:uuid/uuid.dart';
 
 void main() {
   group('User Repository', () {
-
     final email = const Uuid().v4();
     final phone = const Uuid().v4();
 
@@ -16,8 +13,7 @@ void main() {
     test("Test should register a user", () async {
       UserRepository userRepository = UserRepository();
       final user = await userRepository.register(
-          email, 'testpassword', 'testfirst', 'testlast', phone
-      );
+          email, 'testpassword', 'testfirst', 'testlast', phone);
       expect(user.email, email);
       userId = user.id;
     });
@@ -32,7 +28,8 @@ void main() {
     // to test the userlogger repository without a user
     test("Test should create a userlogger", () async {
       UserLoggerRepository userRepository = UserLoggerRepository();
-      final userLogger = await userRepository.create(userId, 'testid', 'testpassword');
+      final userLogger =
+          await userRepository.create(userId, 'testid', 'testpassword');
       expect(userLogger.userId, userId);
       expect(userLogger.loggerId, 'testid');
     });
