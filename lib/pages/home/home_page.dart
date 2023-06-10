ee881164f750d5230be3a39f041e6b778a5e1ea9
marpond <sonkoly.marci02@gmail.com>
2023-06-10 13:18:13 +0200

    data loading is now executed upon selecting the logger and not beforehand. also added fancy progress indicator

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -6,58 +6,86 @@ import 'package:radon_app/models/logger.dart';
 import 'package:radon_app/pages/home/map_widget.dart';
 import 'package:radon_app/pages/home/stats/graphs_widget.dart';
 import 'package:radon_app/repositories/logger_repository.dart';
+import 'package:radon_app/widgets/asap_text.dart';
 import 'package:radon_app/widgets/foo_appbar.dart';
 
 class HomePage extends HookWidget {
-  const HomePage({super.key, required this.selectedLogger});
+  const HomePage({super.key, required this.loggerId});
 
-  final Logger selectedLogger;
+  final String loggerId;
 
   @override
   Widget build(BuildContext context) {
     final bottomNavIndex = useState(0);
 
-    final selectedLoggerState = useState<Logger>(selectedLogger);
+    final loggerRepository = LoggerRepository();
+    final loggerState = useState<Logger?>(null);
 
     useEffect(() {
-      Timer.periodic(const Duration(minutes: 15), (timer) async {
-        debugPrint('Updating logger data');
-        final loggerRepository = LoggerRepository();
-        final logger = await loggerRepository.read(selectedLogger.id);
-        selectedLoggerState.value = logger;
+      if (loggerState.value == null) {
+        debugPrint('${DateTime.now()} - Initial logger read');
+        loggerRepository.read(loggerId).then((logger) {
+          loggerState.value = logger;
+        });
+      }
+      Timer? timer;
+      debugPrint('${DateTime.now()} - Starting timer');
+      timer = Timer.periodic(const Duration(minutes: 15), (_) async {
+        debugPrint('${DateTime.now()} - Updating logger');
+        final logger = await loggerRepository.read(loggerId);
+        loggerState.value = logger;
       });
-      return null;
+
+      return () {
+        debugPrint('${DateTime.now()} - Cancelling timer');
+        timer?.cancel();
+        timer = null;
+      };
     }, []);
 
-    return Scaffold(
-      appBar: FooAppbar(context: context, title: selectedLoggerState.value.id),
-      body: IndexedStack(
-        index: bottomNavIndex.value,
-        children: [
-          SingleChildScrollView(child: GraphsWidget(logger: selectedLoggerState.value)),
-          MapWidget(logger: selectedLoggerState.value),
-        ],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.poll_outlined),
-            label: 'Graphs',
-            activeIcon: Icon(Icons.poll_rounded),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.location_on_outlined),
-            label: 'Map',
-            activeIcon: Icon(Icons.location_on),
-          ),
-        ],
-        selectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold, fontFamily: 'Asap'),
-        unselectedLabelStyle: const TextStyle(fontWeight: FontWeight.normal, fontFamily: 'Asap'),
-        currentIndex: bottomNavIndex.value,
-        onTap: (int index) {
-          bottomNavIndex.value = index;
-        },
-      ),
-    );
+    return loggerState.value == null
+        ? const Scaffold(
+            body: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Center(child: CircularProgressIndicator()),
+                SizedBox(height: 20),
+                Center(child: AsapText(text: 'Loading data...')),
+              ],
+            )
+          )
+        : Scaffold(
+            appBar: FooAppbar(context: context, title: loggerState.value!.id),
+            body: IndexedStack(
+              index: bottomNavIndex.value,
+              children: [
+                SingleChildScrollView(
+                    child: GraphsWidget(logger: loggerState.value!)),
+                MapWidget(logger: loggerState.value!),
+              ],
+            ),
+            bottomNavigationBar: BottomNavigationBar(
+              items: const <BottomNavigationBarItem>[
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.poll_outlined),
+                  label: 'Graphs',
+                  activeIcon: Icon(Icons.poll_rounded),
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.location_on_outlined),
+                  label: 'Map',
+                  activeIcon: Icon(Icons.location_on),
+                ),
+              ],
+              selectedLabelStyle: const TextStyle(
+                  fontWeight: FontWeight.bold, fontFamily: 'Asap'),
+              unselectedLabelStyle: const TextStyle(
+                  fontWeight: FontWeight.normal, fontFamily: 'Asap'),
+              currentIndex: bottomNavIndex.value,
+              onTap: (int index) {
+                bottomNavIndex.value = index;
+              },
+            ),
+          );
   }
 }
